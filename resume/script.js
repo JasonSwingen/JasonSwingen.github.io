70ee90f2a6cd4fc36cf843f21f19957b2f1d0bb4
Jason Swingen <jason.swingen@gmail.com>
2014-12-03 17:43:35 -0600

    changed nav from single to double column

--- resume/script.js
+++ resume/script.js
@@ -5,6 +5,7 @@ $(document).ready(function($) {
     $('html, body').animate({scrollTop: 0}, 900)
   });
 
+  // Smooth scrolling to sections
   $(function() {
     $('a[href*=#]:not([href=#])').click(function() {
       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
