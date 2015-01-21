ffe6b6956f1d8dfb4fcd83757a1c7222ecb01536
Jason Swingen <jason.swingen@gmail.com>
2015-01-21 10:09:50 -0600

    replaced masthead with navigation

--- script.js
+++ script.js
@@ -21,6 +21,21 @@ $(document).ready(function($) {
     });
   });
 
+  $(function() {
+  var menuToggle = $('#js-mobile-menu').unbind();
+  $('#js-navigation-menu').removeClass("show");
+
+  menuToggle.on('click', function(e) {
+    e.preventDefault();
+    $('#js-navigation-menu').slideToggle(function(){
+      if($('#js-navigation-menu').is(':hidden')) {
+        $('#js-navigation-menu').removeAttr('style');
+      }
+    });
+  });
+});
+
+
   // //Fade In
   // $(document).ready(function() {
   //   var element = document.getElementById("js-fadeInElement");
