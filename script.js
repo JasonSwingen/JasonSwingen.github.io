ead98a49a7fa6aaea401f896b154a56eeef90316
Jason Swingen <jason.swingen@gmail.com>
2015-03-10 17:17:15 -0500

    removed map css and js

--- script.js
+++ script.js
@@ -96,57 +96,57 @@ $(document).ready(function($) { // Start of Document Ready Javascript
   }
 
   // Map
-  var bittersMap = (function () {
-    var myLatlng = new google.maps.LatLng(46.7687250,-92.1293140),
-        mapCenter = new google.maps.LatLng(46.7687150,-92.1293040),
-        mapCanvas = document.getElementById('map_canvas'),
-        mapOptions = {
-          center: mapCenter,
-          zoom: 13,
-          scrollwheel: false,
-          draggable: true,
-          disableDefaultUI: true,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        },
-        map = new google.maps.Map(mapCanvas, mapOptions),
-        contentString =
-          '<div id="content">'+
-          '<div id="siteNotice">'+
-          '</div>'+
-          '<h1 id="firstHeading" class="firstHeading">thoughtbot</h1>'+
-          '<div id="bodyContent"'+
-          '<p>Sveavägen 98</p>'+
-          '</div>'+
-          '</div>',
-        infowindow = new google.maps.InfoWindow({
-          content: contentString,
-          maxWidth: 300
-        }),
-        marker = new google.maps.Marker({
-          position: myLatlng,
-          map: map,
-          title: 'thoughtbot (Sweden)'
-        });
+  // var bittersMap = (function () {
+  //   var myLatlng = new google.maps.LatLng(46.7687250,-92.1293140),
+  //       mapCenter = new google.maps.LatLng(46.7687150,-92.1293040),
+  //       mapCanvas = document.getElementById('map_canvas'),
+  //       mapOptions = {
+  //         center: mapCenter,
+  //         zoom: 13,
+  //         scrollwheel: false,
+  //         draggable: true,
+  //         disableDefaultUI: true,
+  //         mapTypeId: google.maps.MapTypeId.ROADMAP
+  //       },
+  //       map = new google.maps.Map(mapCanvas, mapOptions),
+  //       contentString =
+  //         '<div id="content">'+
+  //         '<div id="siteNotice">'+
+  //         '</div>'+
+  //         '<h1 id="firstHeading" class="firstHeading">thoughtbot</h1>'+
+  //         '<div id="bodyContent"'+
+  //         '<p>Sveavägen 98</p>'+
+  //         '</div>'+
+  //         '</div>',
+  //       infowindow = new google.maps.InfoWindow({
+  //         content: contentString,
+  //         maxWidth: 300
+  //       }),
+  //       marker = new google.maps.Marker({
+  //         position: myLatlng,
+  //         map: map,
+  //         title: 'thoughtbot (Sweden)'
+  //       });
 
-    return {
-      init: function () {
-        map.set('styles', [{
-          featureType: 'landscape',
-          elementType: 'geometry',
-          stylers: [
-            { hue: '#ffff00' },
-            { saturation: 30 },
-            { lightness: 10}
-          ]}
-        ]);
+  //   return {
+  //     init: function () {
+  //       map.set('styles', [{
+  //         featureType: 'landscape',
+  //         elementType: 'geometry',
+  //         stylers: [
+  //           { hue: '#ffff00' },
+  //           { saturation: 30 },
+  //           { lightness: 10}
+  //         ]}
+  //       ]);
 
-        google.maps.event.addListener(marker, 'click', function () {
-          infowindow.open(map,marker);
-        });
-      }
-    };
-  }());
-  bittersMap.init();
+  //       google.maps.event.addListener(marker, 'click', function () {
+  //         infowindow.open(map,marker);
+  //       });
+  //     }
+  //   };
+  // }());
+  // bittersMap.init();
 
 }); // End of Document Ready Javascript
 
