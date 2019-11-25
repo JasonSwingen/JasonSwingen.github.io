ad1c617b5d3a4e7603ae65370d16721581ebacca
Jason Swingen <jason.swingen@gmail.com>
2019-11-25 13:50:27 -0600

    changed face in timing

--- js/freelancer.js
+++ js/freelancer.js
@@ -39,7 +39,7 @@ $(document).ready(function() {
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             /* If the object is completely visible in the window, fade it it */
             if( bottom_of_window > bottom_of_object ){
-                $(this).animate({'opacity':'1'},13);
+                $(this).animate({'opacity':'1'},2);
             }
         });
     });
