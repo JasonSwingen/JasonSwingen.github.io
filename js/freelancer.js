dd24aef1d48fded111d0473c626bbd81241dd058
Jason Swingen <jason.swingen@gmail.com>
2022-01-27 13:12:29 -0600

    js changes

--- js/freelancer.js
+++ js/freelancer.js
@@ -70,6 +70,38 @@ $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
 
+// Appear on scroll animations
+const faders = document.querySelectorAll(".fade-in");
+const sliders = document.querySelectorAll(".slide-in");
+
+const appearOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -250px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver(function(
+  entries,
+  appearOnScroll
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+});
+
+sliders.forEach(slider => {
+  appearOnScroll.observe(slider);
+});
+
 // Keeps visitors from accidently backing out when trying to close modal
 //Modal Closer With Back Button Support (Uses EventDelegation, so it works for ajax loaded content too.)
 (function() {
